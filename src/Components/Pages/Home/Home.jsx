f68a7bb35543f0daee6bb5acb93382df849adfb4
detectiveseo <detectiveseo1@gmail.com>
2023-06-08 12:28:15 +0600

    review section

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import CategorySlider from './Category/CategorySlider';
 import Banner from './Banner/Banner';
 import PopularMenu from './PopularMenu/PopularMenu';
 import Feature from './Features/Feature';
+import Reviews from './Reviews/Reviews';
 
 const Home = () => {
 
@@ -11,6 +12,7 @@ const Home = () => {
             <CategorySlider />
             <PopularMenu />
             <Feature />
+            <Reviews />
         </div>
     );
 };
---
+++ src/Components/Pages/Home/Reviews/Reviews.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import SectionTitle from '../../../Shared/SectionTitle';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation, Autoplay } from "swiper";
+import quote from '../../../../assets/icon/quote-left 1.png'
+
+
+const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("reviews.json")
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+
+    console.log(reviews);
+    return (
+        <div>
+            <SectionTitle heading="What Our Clients Say" subtitle="TESTIMONIALS" />
+
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                // pagination={{
+                //     clickable: true,
+                // }}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                navigation={true}
+                modules={[Autoplay, Navigation]}
+                className="mySwiper"
+            >
+                {reviews.map(review => {
+                    return (
+                        <SwiperSlide key={review._id}>
+                            <div className='flex flex-col justify-center items-center w-4/12 mx-auto text-center gap-6'>
+                                <div><img src={quote} alt="" /></div>
+                                <h4>{review.details}</h4>
+                                <h4 className='text-4xl font-bold'>{review.name}</h4>
+                            </div>
+                        </SwiperSlide>
+                    )
+                })}
+            </Swiper>
+        </div>
+    );
+};
+
+export default Reviews;
