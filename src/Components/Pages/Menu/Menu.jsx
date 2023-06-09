8f611fb9b02cbb72f93a901964001921e23b951f
detectiveseo <detectiveseo1@gmail.com>
2023-06-09 18:45:40 +0600

    added section for menu

--- src/Components/Pages/Menu/Menu.jsx
+++ src/Components/Pages/Menu/Menu.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import SectionBanner from '../../Shared/SectionBenner/SectionBanner';
 import backgorundImg from '../../../assets/menu/banner3.jpg'
+import SectionTitle from '../../Shared/SectionTitle';
+import MenuCard from '../../Shared/MenuCard/MenuCard';
 
 const Menu = () => {
+    const [menu, setMenu] = useState([]);
+    useEffect(() => {
+        fetch("menu.json")
+            .then((res) => res.json())
+            .then((data) => {
+                const popularItems = data.filter(x => x.category == "popular");
+                setMenu(popularItems)
+            });
+    }, [])
+
     return (
         <div>
             <Helmet>
                 <title>Bistro boss | Menu</title>
             </Helmet>
+
+
             <SectionBanner backgroundImage={backgorundImg} heading={"our menu"} subtitle={"Would you like to try a dish?"}></SectionBanner>
+
+
+            <SectionTitle subtitle={"Don't miss"} heading={"TODAY'S OFFER"}></SectionTitle>
+            
+            <MenuCard menu={menu}></MenuCard>
         </div>
     );
 };
---
+++ src/Components/Shared/Buttons/BottomOutletBtn.jsx
@@ -0,0 +1,9 @@
+const BottomOutletBtn = ({children}) => {
+    return (
+        <button>
+            {children}
+        </button>
+    );
+};
+
+export default BottomOutletBtn;
